1024277f480b7f69fd563998048e4906504ea066
Quinten <43782037+802818@users.noreply.github.com>
2018-11-20 12:48:24 -0500

    Template v 1.0w0
    Still platformer with side scroller physics, same as template, BUT now it also has a BOX! Which can move! Also heres a buton that doesn't do anything but has a pressed animation.

--- main.js
+++ main.js
@@ -1,387 +1,161 @@
-// =============================================================================
-// sprites
-// =============================================================================
 
-//
-// hero sprite
-//
-function Hero(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'hero');
-    this.anchor.set(0.5, 0.5);
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-    // physic properties
-    this.game.physics.enable(this);
-    this.body.collideWorldBounds = true;
+function preload() {
 
-   //// this.animations.add('stop', [0]);
-    //this.animations.add('run', [1, 2], 8, true); // 8fps looped
-   // this.animations.add('jump', [3]);
-   // this.animations.add('fall', [4]);
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('box', 'assets/box.png');
+    game.load.spritesheet('button', 'assets/Button (1).png', 63, 26);
 }
 
-// inherit from Phaser.Sprite
-Hero.prototype = Object.create(Phaser.Sprite.prototype);
-Hero.prototype.constructor = Hero;
+var player;
+var platforms;
+var cursors;
+var objects;
+var tick = 0;
+var stars;
+var score = 0;
+var scoreText;
+var ground;
+var Button;
+var box;
+    var press = false;
+function create() {
 
-Hero.prototype.move = function (direction) {
-    const SPEED = 200;
-    this.body.velocity.x = direction * SPEED;
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    // update image flipping & animations
-    if (this.body.velocity.x < 0) {
-        this.scale.x = -1;
-    }
-    else if (this.body.velocity.x > 0) {
-        this.scale.x = 1;
-    }
-};
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
 
-Hero.prototype.jump = function () {
-    const JUMP_SPEED = 600;
-    let canJump = this.body.touching.down;
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+    Button = game.add.sprite(600, game.world.height - 58, 'button');
+    Button.enableBody = true;
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
+    
+    objects = game.add.group();
+    objects.enableBody = true;
+    
+    box = objects.create(140, game.world.height - 106, 'box');
+    box.body.gravity.y = 300;
+    box.body.drag.x = 500;
+    box.collideWorldBounds - true;
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 32, 'ground');
 
-    if (canJump) {
-        this.body.velocity.y = -JUMP_SPEED;
-    }
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 1);
 
-    return canJump;
-};
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
 
-Hero.prototype.bounce = function () {
-    const BOUNCE_SPEED = 200;
-    this.body.velocity.y = -BOUNCE_SPEED;
-};
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
+    ledge.body.immovable = true;
 
-Hero.prototype.update = function () {
-    // update sprite animation, if it needs changing
-    let animationName = this._getAnimationName();
-    if (this.animations.name !== animationName) {
-        this.animations.play(animationName);
-    }
-};
+    ledge = platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
 
-Hero.prototype._getAnimationName = function () {
-    let name = 'stop'; // default animation
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
 
-    // jumping
-    if (this.body.velocity.y < 0) {
-        name = 'jump';
-    }
-    // falling
-    else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
-        name = 'fall';
-    }
-    else if (this.body.velocity.x !== 0 && this.body.touching.down) {
-        name = 'run';
-    }
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
+    game.physics.arcade.enable(Button);
 
-    return name;
-};
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
-//
-// Spider (enemy)
-//
-function Spider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'spider');
 
-    // anchor
-    this.anchor.set(0.5);
-    // animation
-    //this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
-   // this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
-   // this.animations.play('crawl');
+    
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    // physic properties
-    this.game.physics.enable(this);
-    this.body.collideWorldBounds = true;
-    this.body.velocity.x = Spider.SPEED;
+    //  The score
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
+    
 }
 
-Spider.SPEED = 100;
+function update() {
 
-// inherit from Phaser.Sprite
-Spider.prototype = Object.create(Phaser.Sprite.prototype);
-Spider.prototype.constructor = Spider;
+    //  Collide the player and the stars with the platforms
+    var hitPlatform = game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    var hitObject = game.physics.arcade.collide(player, objects);
+    game.physics.arcade.collide(box, platforms);
+    //game.physics.arcade.overlap(Button, box, pressed, null, this);
+    
+    //game.physics.arcade.overlap(Button, player, pressed, null, this);
+    
+    pressed();
+    tick++;
+    game.physics.arcade.overlap(player, objects, pushStuff, null, this);
 
-Spider.prototype.update = function () {
-    // check against walls and reverse direction if necessary
-    if (this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -Spider.SPEED; // turn left
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
     }
-    else if (this.body.touching.left || this.body.blocked.left) {
-        this.body.velocity.x = Spider.SPEED; // turn right
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
     }
-};
+    else
+    {
+        //  Stand still
+        player.animations.stop();
 
-Spider.prototype.die = function () {
-    this.body.enable = false;
-
-    this.animations.play('die').onComplete.addOnce(function () {
-        this.kill();
-    }, this);
-};
-
-
-// =============================================================================
-// game states
-// =============================================================================
-
-PlayState = {};
-
-const LEVEL_COUNT = 2;
-
-PlayState.init = function (data) {
-    this.game.renderer.renderSession.roundPixels = true;
-
-    this.keys = this.game.input.keyboard.addKeys({
-        left: Phaser.KeyCode.LEFT,
-        right: Phaser.KeyCode.RIGHT,
-        up: Phaser.KeyCode.UP
-    });
-
-    this.keys.up.onDown.add(function () {
-        let didJump = this.hero.jump();
-        if (didJump) {
-            this.sfx.jump.play();
-        }
-    }, this);
-
-    this.coinPickupCount = 0;
-    this.hasKey = false;
-    this.level = (data.level || 0) % LEVEL_COUNT;
-};
-
-PlayState.preload = function () {
-    this.game.load.json('level:0', 'data/level01.json');
-    this.game.load.json('level:1', 'data/level01.json');
-
-    this.game.load.image('font:numbers', 'images/numbers.png');
-
-    this.game.load.image('background', 'images/background.png');
-    this.game.load.image('ground', 'images/ground.png');
-    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
-    this.game.load.image('invisible-wall', 'images/invisible_wall.png');
-    this.game.load.image('icon:coin', 'images/coin_icon.png');
-    this.game.load.image('key', 'images/key.png');
-    this.game.load.image('hero', 'images/hero.png');
-
-   this.game.load.image('spider', 'images/spider.png');
-    this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
-    this.game.load.spritesheet('door', 'images/door.png', 42, 66);
-    this.game.load.spritesheet('icon:key', 'images/key_icon.png', 34, 30);
-
-    this.game.load.audio('sfx:jump', 'audio/jump.wav');
-    this.game.load.audio('sfx:coin', 'audio/coin.wav');
-    this.game.load.audio('sfx:stomp', 'audio/stomp.wav');
-    this.game.load.audio('sfx:key', 'audio/key.wav');
-    this.game.load.audio('sfx:door', 'audio/door.wav');
-};
-
-PlayState.create = function () {
-    // create sound entities
-    this.sfx = {
-        jump: this.game.add.audio('sfx:jump'),
-        coin: this.game.add.audio('sfx:coin'),
-        stomp: this.game.add.audio('sfx:stomp'),
-        key: this.game.add.audio('sfx:key'),
-        door: this.game.add.audio('sfx:door')
-    };
-
-    // create level
-    this.game.add.image(0, 0, 'background');
-    this._loadLevel(this.game.cache.getJSON(`level:${this.level}`));
-
-    this._createHud();
-};
-
-PlayState.update = function () {
-    this._handleCollisions();
-    this._handleInput();
-
-    this.coinFont.text = `x${this.coinPickupCount}`;
-    this.keyIcon.frame = this.hasKey ? 1 : 0;
-};
-
-PlayState._handleCollisions = function () {
-    this.game.physics.arcade.collide(this.spiders, this.platforms);
-    this.game.physics.arcade.collide(this.spiders, this.enemyWalls);
-    this.game.physics.arcade.collide(this.hero, this.platforms);
-
-    this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
-        null, this);
-    this.game.physics.arcade.overlap(this.hero, this.spiders,
-        this._onHeroVsEnemy, null, this);
-    this.game.physics.arcade.overlap(this.hero, this.key, this._onHeroVsKey,
-        null, this);
-    this.game.physics.arcade.overlap(this.hero, this.door, this._onHeroVsDoor,
-        // ignore if there is no key or the player is on air
-        function (hero, door) {
-            return this.hasKey && hero.body.touching.down;
-        }, this);
-};
-
-PlayState._handleInput = function () {
-    if (this.keys.left.isDown) { // move hero left
-        this.hero.move(-1);
+        player.frame = 4;
     }
-    else if (this.keys.right.isDown) { // move hero right
-        this.hero.move(1);
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+    {
+        player.body.velocity.y = -350;
     }
-    else { // stop
-        this.hero.move(0);
+
+}
+
+function pushStuff (player, objects) {
+    
+}
+
+function pressed () {
+console.log("run");
+
+    if (game.physics.arcade.overlap(Button, player)) {
+        console.log("Hello");
+        Button.frame = 1;
+        press = true;    
     }
-};
-
-PlayState._loadLevel = function (data) {
-    // create all the groups/layers that we need
-    this.bgDecoration = this.game.add.group();
-    this.platforms = this.game.add.group();
-    this.coins = this.game.add.group();
-    this.spiders = this.game.add.group();
-    this.enemyWalls = this.game.add.group();
-    this.enemyWalls.visible = false;
-
-    // spawn all platforms
-    data.platforms.forEach(this._spawnPlatform, this);
-    // spawn hero and enemies
-    this._spawnCharacters({hero: data.hero, spiders: data.spiders});
-    // spawn important objects
-    data.coins.forEach(this._spawnCoin, this);
-    this._spawnDoor(data.door.x, data.door.y);
-    this._spawnKey(data.key.x, data.key.y);
-
-    // enable gravity
-    const GRAVITY = 1200;
-    this.game.physics.arcade.gravity.y = GRAVITY;
-};
-
-PlayState._spawnPlatform = function (platform) {
-    let sprite = this.platforms.create(
-        platform.x, platform.y, platform.image);
-
-    this.game.physics.enable(sprite);
-    sprite.body.allowGravity = false;
-    sprite.body.immovable = true;
-
-    this._spawnEnemyWall(platform.x, platform.y, 'left');
-    this._spawnEnemyWall(platform.x + sprite.width, platform.y, 'right');
-};
-
-PlayState._spawnEnemyWall = function (x, y, side) {
-    let sprite = this.enemyWalls.create(x, y, 'invisible-wall');
-    // anchor and y displacement
-    sprite.anchor.set(side === 'left' ? 1 : 0, 1);
-    // physic properties
-    this.game.physics.enable(sprite);
-    sprite.body.immovable = true;
-    sprite.body.allowGravity = false;
-};
-
-PlayState._spawnCharacters = function (data) {
-    // spawn spiders
-    data.spiders.forEach(function (spider) {
-        let sprite = new Spider(this.game, spider.x, spider.y);
-        this.spiders.add(sprite);
-    }, this);
-
-    // spawn hero
-    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
-    this.game.add.existing(this.hero);
-};
-
-PlayState._spawnCoin = function (coin) {
-    let sprite = this.coins.create(coin.x, coin.y, 'coin');
-    sprite.anchor.set(0.5, 0.5);
-
-    this.game.physics.enable(sprite);
-    sprite.body.allowGravity = false;
-
-    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
-    sprite.animations.play('rotate');
-};
-
-PlayState._spawnDoor = function (x, y) {
-    this.door = this.bgDecoration.create(x, y, 'door');
-    this.door.anchor.setTo(0.5, 1);
-    this.game.physics.enable(this.door);
-    this.door.body.allowGravity = false;
-};
-
-PlayState._spawnKey = function (x, y) {
-    this.key = this.bgDecoration.create(x, y, 'key');
-    this.key.anchor.set(0.5, 0.5);
-    // enable physics to detect collisions, so the hero can pick the key up
-    this.game.physics.enable(this.key);
-    this.key.body.allowGravity = false;
-    // add a small 'up & down' animation via a tween
-    this.key.y -= 3;
-    this.game.add.tween(this.key)
-        .to({y: this.key.y + 6}, 800, Phaser.Easing.Sinusoidal.InOut)
-        .yoyo(true)
-        .loop()
-        .start();
-};
-
-
-PlayState._onHeroVsCoin = function (hero, coin) {
-    this.sfx.coin.play();
-    coin.kill();
-    this.coinPickupCount++;
-};
-
-PlayState._onHeroVsEnemy = function (hero, enemy) {
-    if (hero.body.velocity.y > 0) { // kill enemies when hero is falling
-        hero.bounce();
-        enemy.die();
-        this.sfx.stomp.play();
+    else if (game.physics.arcade.overlap(Button, box)) {
+        console.log("Hello");
+        Button.frame = 1;
+        press = true;
     }
-    else { // game over -> restart the game
-        this.sfx.stomp.play();
-        this.game.state.restart(true, false, {level: this.level});
+    else {
+        console.log("bye");
+        press = false;
+        Button.frame = 0;
     }
-};
-
-PlayState._onHeroVsKey = function (hero, key) {
-    this.sfx.key.play();
-    key.kill();
-    this.hasKey = true;
-};
-
-PlayState._onHeroVsDoor = function (hero, door) {
-    this.sfx.door.play();
-    this.game.state.restart(true, false, { level: this.level + 1 });
-};
-
-PlayState._createHud = function () {
-    const NUMBERS_STR = '0123456789X ';
-    this.coinFont = this.game.add.retroFont('font:numbers', 20, 26,
-        NUMBERS_STR);
-
-    this.keyIcon = this.game.make.image(0, 19, 'icon:key');
-    this.keyIcon.anchor.set(0, 0.5);
-
-    let coinIcon = this.game.make.image(this.keyIcon.width + 7, 0, 'icon:coin');
-    let coinScoreImg = this.game.make.image(coinIcon.x + coinIcon.width,
-        coinIcon.height / 2, this.coinFont);
-    coinScoreImg.anchor.set(0, 0.5);
-
-    this.hud = this.game.add.group();
-    this.hud.add(coinIcon);
-    this.hud.add(coinScoreImg);
-    this.hud.add(this.keyIcon);
-    this.hud.position.set(10, 10);
-};
-
-// =============================================================================
-// entry point
-// =============================================================================
-
-window.onload = function () {
-    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
-    game.state.add('play', PlayState);
-    game.state.start('play', true, false, {level: 0});
-};
+}
